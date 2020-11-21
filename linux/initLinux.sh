81573d6e6eb16487beb2cfe61c635ec7cc53045a
explore <tfyt2009@sina.cn>
2020-11-21 13:31:25 +0800

    增加说明文档

--- linux/initLinux.sh
+++ linux/initLinux.sh
@@ -1,21 +1,32 @@
 #! bin/bash
+
 # 升级内核版本
 yum update kernel -y 
-# ——————————————————————————防火墙——————————————————————————
+
 # 关闭防火墙
 systemctl stop firewalld
 # 取消开机自启
 systemctl disable firewalld
 
-# ——————————————————————————虚拟内存——————————————————————————
+# 虚拟内存
 swapoff -a
 
-# ——————————————————————————登陆欢迎语——————————————————————————
-# ——————————————————————————shell快捷键——————————————————————————
+# 设置时区
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+# 安装常用工具
+yum install -y wget vim net-tools
+
+# 登陆欢迎语
 cat <<EOF> /etc/motd
 
 Welcome to CentOS 7 !
 
+EOF
+
+# shell提示符前缀
+cat <<EOF>> /etc/bashrc
+PS1="[\u@\h \W]\\$ "
 EOF
 
 # ——————————————————————————shell快捷键——————————————————————————
@@ -24,20 +35,10 @@ alias cls="clear"
 alias ll='ls -hl --time-style "+%Y/%m/%d %H:%M"'
 EOF
 
-# if [ `grep -c "^alias cls=" /etc/bashrc` -ne '0' ];then
-# 	echo 'alias cls="clear"' >> /etc/bashrc
-# fi
 
-# if [ `grep -c "^alias ll=" /etc/bashrc`  -ne '0' ];then
-# 	echo -e alias ll=\'ls -hl --time-style "+%Y/%m/%d %H:%M"\' >> /etc/bashrc
-# fi
-
-# 设置本地变量
+# 设置本地变量（shell脚本中文乱码、控制台输出中文乱码、vim中文乱码）
 cat <<EOF> /etc/locale.conf
 LC_ALL=en_US.utf8 
 LC_CTYPE=en_US.utf8 
 LANG=en_US.utf8
 EOF
-
-# s设置时区
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
