a927318d2ba86067319604f85c4f5f700fafdab7
explore <tfyt2009@sina.cn>
2020-11-22 11:44:18 +0800

    新增nginx模块

---
+++ nginx/installNginx.sh
@@ -0,0 +1,14 @@
+#! bin/bash
+# 下载自定义Java安装包
+wget -P /usr/local/src -N http://cdn.ai-brain.cn/bin/nginx/nginx.tar.gz
+# 解压文件
+tar --no-same-owner -zxvf /usr/local/src/nginx.tar.gz -C /usr/local
+
+# 配置环境变量
+grep "^export NGINX_HOME" /etc/profile;
+if (( $? > 0 ))
+then
+echo 'export NGINX_HOME=/usr/local/nginx' >> /etc/profile
+echo 'export PATH=${NGINX_HOME}/bin:$PATH' >> /etc/profile
+source /etc/profile
+fi
