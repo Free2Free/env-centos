b5b06cf7612bc96f14328895c735d2718f517bde
explore <tfyt2009@sina.cn>
2020-11-23 23:00:48 +0800

    修改集群IP

--- zookeeper/installCluster.sh
+++ zookeeper/installCluster.sh
@@ -1,10 +1,10 @@
 #! bin/bash
 # 新增集群主机
-cat <<EOF> /etc/profile.d/es.sh
-export node01=192.168.199.165
-export node02=192.168.199.166
-export node03=192.168.199.167
-EOF
+# cat <<EOF> /etc/profile.d/es.sh
+# export node01=192.168.199.165
+# export node02=192.168.199.166
+# export node03=192.168.199.167
+# EOF
 
 # 新增集群主机
 cat <<EOF> /etc/hosts
@@ -19,12 +19,12 @@ EOF
 # 刷新环境变量
 source /etc/profile
 # 将主机名作为myid的值
-echo ${HOSTNAME} | grep -Po '\d+' > ${ZK_HOME}/data/myid
+echo ${HOSTNAME} | grep -Po '[1-9]+' > ${ZK_HOME}/data/myid
 
 
 cat <<EOF>> ${ZK_HOME}/conf/zoo.cfg
 # myid里面配置的字符串
-server.01=${node01}:2888:3888 
-server.02=${node02}:2888:3888 
-server.03=${node03}:2888:3888 
+server.1=node01:2888:3888 
+server.2=node02:2888:3888 
+server.3=node03:2888:3888 
 EOF
