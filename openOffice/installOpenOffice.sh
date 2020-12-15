6aae5224a993e32787d9bd26db6fc20cb643caac
explore <tfyt2009@sina.cn>
2020-12-15 17:28:34 +0800

    新增openOffice执行客户端

--- openOffice/installOpenOffice.sh
+++ openOffice/installOpenOffice.sh
@@ -8,5 +8,30 @@ rpm -ivh /tmp/openOffice/RPMS/*.rpm
 
 rpm -ivh /tmp/openOffice/RPMS/desktop-integration/openoffice4.1.8-redhat-menus-4.1.8-9803.noarch.rpm
 
+# 创建软连接快捷键
+ln -s /opt/openoffice4/program/soffice /usr/local/bin/soffice
 
+yum install libXext.x86_64 -y
 
+yum groupinstall "X Window System" -y
+
+/opt/openoffice4/program/soffice -headless -accept="socket,host=127.0.0.1,port=8100;urp;" -nofirststartwizard &
+
+ps -ef | grep soffice
+
+# 安装中文字体
+wget -P /usr/local/src -N http://cdn.ai-brain.cn/bin/chineseFont/chineseFont.tar.gz
+# 解压文件
+tar --no-same-owner -zxvf /usr/local/src/chineseFont.tar.gz -C /usr/share/fonts
+
+fc-cache
+
+wget -P /usr/local/src -N http://cdn.ai-brain.cn/bin/jodconverter/jodconverter.tar.gz
+# 解压文件
+tar --no-same-owner -zxvf /usr/local/src/jodconverter.tar.gz -C /usr/local
+
+# 配置环境变量
+sed -i '/JODC_HOME/d' /etc/profile
+echo 'export JODC_HOME=/usr/local/git' >> /etc/profile
+echo 'export PATH=${JODC_HOME}:$PATH' >> /etc/profile
+source /etc/profile
