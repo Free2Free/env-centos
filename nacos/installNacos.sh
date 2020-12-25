310879f0edb4af8023bbc374bc1196a43445a9b1
explore <tfyt2009@sina.cn>
2020-12-25 23:42:45 +0800

    redis开机自启

--- nacos/installNacos.sh
+++ nacos/installNacos.sh
@@ -11,3 +11,21 @@ tar --no-same-owner -zxvf /usr/local/src/nacos.tar.gz -C /usr/local
 # source /etc/profile
 
 # 注册为系统服务
+cat <<EOF> /usr/lib/systemd/system/nacos.service
+[Unit]
+Description=Nacos Server Manager
+After=syslog.target network.target
+
+[Service]
+Type=forking
+# PIDFile=/var/run/redis_6379.pid
+ExecStart=/usr/local/nacos/bin/startup.sh -m standalone
+# ExecReload=/bin/kill -USR2 $MAINPID
+# ExecStop=/bin/kill -SIGINT $MAINPID
+ExecStop=/usr/local/nacos/bin/shutdown.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable nacos
