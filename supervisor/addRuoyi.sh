7ceec7486e666836d90c040625e33f7ab9b80875
explore <tfyt2009@sina.cn>
2020-12-27 23:27:35 +0800

    ruoyi模块

--- supervisor/addRuoyi.sh
+++ supervisor/addRuoyi.sh
@@ -2,7 +2,7 @@
 cat <<EOF> /etc/supervisord.d/ruoyi-auth.ini
 [program:ruoyi-auth]
 directory=${RUOYI}
-command=/usr/local/java/bin/java -Xms256m -Xmx512m -jar ruoyi-auth/target/ruoyi-auth-2.4.0.jar
+command=/usr/local/java/bin/java -Xms128m -Xmx256m -jar ruoyi-auth/target/ruoyi-auth-2.4.0.jar
 autostart=true
 autorestart=true
 startsecs=5
@@ -17,7 +17,7 @@ supervisorctl restart ruoyi-auth
 cat <<EOF> /etc/supervisord.d/ruoyi-gateway.ini
 [program:ruoyi-gateway]
 directory=${RUOYI}
-command=/usr/local/java/bin/java -Xms256m -Xmx512m -jar ruoyi-gateway/target/ruoyi-gateway-2.4.0.jar
+command=/usr/local/java/bin/java -Xms128m -Xmx256m -jar ruoyi-gateway/target/ruoyi-gateway-2.4.0.jar
 autostart=true
 autorestart=true
 startsecs=5
@@ -33,7 +33,7 @@ supervisorctl restart ruoyi-gateway
 cat <<EOF> /etc/supervisord.d/ruoyi-system.ini
 [program:ruoyi-system]
 directory=${RUOYI}
-command=/usr/local/java/bin/java -Xms256m -Xmx512m -jar ruoyi-modules/ruoyi-system/target/ruoyi-modules-system-2.4.0.jar
+command=/usr/local/java/bin/java -Xms128m -Xmx256m -jar ruoyi-modules/ruoyi-system/target/ruoyi-modules-system-2.4.0.jar
 autostart=true
 autorestart=true
 startsecs=5
@@ -43,5 +43,58 @@ redirect_stderr=true
 ;stderr_logfile=/usr/local/nginx/logs/error.log
 EOF
 supervisorctl restart ruoyi-system
+
+
+
+cat <<EOF> /etc/supervisord.d/ruoyi-visual.ini
+[program:ruoyi-visual]
+directory=${RUOYI}
+command=/usr/local/java/bin/java -Xms128m -Xmx256m -jar ruoyi-visual/ruoyi-monitor/target/ruoyi-visual-monitor-2.4.0.jar
+autostart=true
+autorestart=true
+startsecs=5
+startretries=3
+redirect_stderr=true
+;stdout_logfile=/usr/local/nginx/logs/access.log
+;stderr_logfile=/usr/local/nginx/logs/error.log
+EOF
+supervisorctl restart ruoyi-visual
+
+
+
+cat <<EOF> /etc/supervisord.d/ruoyi-job.ini
+[program:ruoyi-job]
+directory=${RUOYI}
+command=/usr/local/java/bin/java -Xms128m -Xmx256m -jar ruoyi-modules/ruoyi-job/target/ruoyi-modules-job-2.4.0.jar
+autostart=true
+autorestart=true
+startsecs=5
+startretries=3
+redirect_stderr=true
+;stdout_logfile=/usr/local/nginx/logs/access.log
+;stderr_logfile=/usr/local/nginx/logs/error.log
+EOF
+supervisorctl restart ruoyi-job
+
+
+
+
+cat <<EOF> /etc/supervisord.d/ruoyi-gen.ini
+[program:ruoyi-gen]
+directory=${RUOYI}
+command=/usr/local/java/bin/java -Xms128m -Xmx256m -jar ruoyi-modules/ruoyi-gen/target/ruoyi-modules-gen-2.4.0.jar
+autostart=true
+autorestart=true
+startsecs=5
+startretries=3
+redirect_stderr=true
+;stdout_logfile=/usr/local/nginx/logs/access.log
+;stderr_logfile=/usr/local/nginx/logs/error.log
+EOF
+supervisorctl restart ruoyi-gen
+
+
+
+
 
 
