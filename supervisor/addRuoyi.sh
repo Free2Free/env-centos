33fed3f9ff970dd876388923429257e0af346683
explore <tfyt2009@sina.cn>
2020-12-27 23:06:16 +0800

    ruoyi模块

--- supervisor/addRuoyi.sh
+++ supervisor/addRuoyi.sh
@@ -2,7 +2,7 @@
 cat <<EOF> /etc/supervisord.d/ruoyi-auth.ini
 [program:ruoyi-auth]
 directory=${RUOYI}
-command=/usr/local/java/bin/java -jar ruoyi-auth/target/ruoyi-auth-2.4.0.jar
+command=/usr/local/java/bin/java -Xms256m -Xmx512m -jar ruoyi-auth/target/ruoyi-auth-2.4.0.jar
 autostart=true
 autorestart=true
 startsecs=5
@@ -17,7 +17,7 @@ supervisorctl restart ruoyi-auth
 cat <<EOF> /etc/supervisord.d/ruoyi-gateway.ini
 [program:ruoyi-gateway]
 directory=${RUOYI}
-command=/usr/local/java/bin/java -jar ruoyi-gateway/target/ruoyi-gateway-2.4.0.jar
+command=/usr/local/java/bin/java -Xms256m -Xmx512m -jar ruoyi-gateway/target/ruoyi-gateway-2.4.0.jar
 autostart=true
 autorestart=true
 startsecs=5
@@ -33,7 +33,7 @@ supervisorctl restart ruoyi-gateway
 cat <<EOF> /etc/supervisord.d/ruoyi-system.ini
 [program:ruoyi-system]
 directory=${RUOYI}
-command=/usr/local/java/bin/java -jar ruoyi-modules/ruoyi-system/target/ruoyi-modules-system-2.4.0.jar
+command=/usr/local/java/bin/java -Xms256m -Xmx512m -jar ruoyi-modules/ruoyi-system/target/ruoyi-modules-system-2.4.0.jar
 autostart=true
 autorestart=true
 startsecs=5
