641416348097ace860b6c3b25f39f85aacc66c73
explore <tfyt2009@sina.cn>
2020-12-29 19:19:50 +0800

    新增mongodb模块

--- supervisor/untitled.sh
+++
@@ -1,9 +0,0 @@
-cat <<EOF> etc/supervisord.d/redis.ini
-[program:redis]
-command=/usr/local/redis/bin/redis-server /usr/local/redis/conf/redis.conf --daemonize no
-autostart=true
-autorestart=true
-startsecs=5
-startretries=3
-redirect_stderr=true
-EOF
