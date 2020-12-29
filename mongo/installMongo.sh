77d710bca7fedfaa702556d5a350878781fdf2e7
explore <tfyt2009@sina.cn>
2020-12-29 22:02:40 +0800

    修改mongo模块

--- mongo/installMongo.sh
+++ mongo/installMongo.sh
@@ -1,11 +1,11 @@
 #! bin/bash
 # 下载自定义Java安装包
-wget -P /usr/local/src -N http://cdn.ai-brain.cn/bin/mongodb/mongodb.tar.gz
+wget -P /usr/local/src -N http://cdn.ai-brain.cn/bin/mongo/mongo.tar.gz
 # 解压文件
-tar --no-same-owner -zxvf /usr/local/src/mongodb.tar.gz -C /usr/local
+tar --no-same-owner -zxvf /usr/local/src/mongo.tar.gz -C /usr/local
 
 # 配置环境变量
 sed -i '/MONGO_HOME/d' /etc/profile
-echo 'export MONGO_HOME=/usr/local/mongodb' >> /etc/profile
+echo 'export MONGO_HOME=/usr/local/mongo' >> /etc/profile
 echo 'export PATH=${MONGO_HOME}/bin:$PATH' >> /etc/profile
 source /etc/profile
