f61abe28060bb5d2287bcde4bba018c43617169b
explore <tfyt2009@sina.cn>
2021-01-14 17:42:57 +0800

    修复gitbook

--- gitbook/installGiBbook.sh
+++ gitbook/installGiBbook.sh
@@ -2,7 +2,9 @@
 mkdir -p /opt/gitbooks
 cd /opt/gitbooks
 npm install -g gitbook-cli
+cd /usr/local/nodejs/lib/node_modules/gitbook-cli/node_modules/npm/node_modules
 npm install graceful-fs
+cd /opt/gitbooks
 gitbook fetch
 gitbook init
 
