74936a22d5c6c1b4e957f97ac5192fdb8bc8a310
explore <ireliaashe@163.com>
2021-10-25 19:22:23 +0800

    更新yum国内源

--- linux/initLinux.sh
+++ linux/initLinux.sh
@@ -1,8 +1,5 @@
 #! bin/bash
 
-# 升级内核版本(-y表示自动安装不需要手动确认)
-yum update kernel -y
-
 # 关闭防火墙
 systemctl stop firewalld
 # 取消开机自启
@@ -14,8 +11,6 @@ swapoff -a
 # 设置时区
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
-# 安装常用工具
-yum install -y wget vim unzip net-tools bash-completion lsof ntp openssl-devel
 
 # 登陆欢迎语
 cat <<EOF >/etc/motd
@@ -48,4 +43,22 @@ LC_CTYPE=en_US.utf8
 LANG=en_US.utf8
 EOF
 
-source /etc/profile
+# 修改yum为国内源
+# 备份原yum源
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bck
+# CentOS7更新CentOS-Base yum源
+wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+# 替换版本号（如果上述正常，可以忽略此步骤）
+sed -i 's/$releasever/7/g' /etc/yum.repos.d/CentOS-Base.repo
+# 清理yum缓存
+yum clean all
+# 缓存软件包信息
+yum makecache
+# 更新
+yum update
+
+# 安装常用工具
+yum install -y wget vim unzip net-tools bash-completion lsof ntp openssl-devel
+
+# 升级内核版本(-y表示自动安装不需要手动确认)
+yum update kernel -y
